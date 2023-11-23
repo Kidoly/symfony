2309120f5ab03bc153d6d098001a396388aae341
Kidoly <mistercreepreal@gmail.com>
2023-11-23 14:21:47 +0100

    Cats from files

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,25 +3,51 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Finder\Finder;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        $finder= new Finder();
+        //Ajouter un formulaire pour créer un dossier
+        $form = $this->createFormBuilder()
+            ->add('nom', TextType::class, ["label" => "Nom du dossier"])
+            ->add('ok', SubmitType::class, ["label" => "OK"])
+            ->getForm();
+
+        //Gestion du retour du formulaire
+        //1- Récupérer les données du formulaire
+        $form->handleRequest($request);
+        //2- Vérifier si le formulaire est soumis et valide
+        if ($form->isSubmitted() && $form->isValid()) {
+            //3- Récupérer les données du formulaire
+            $nomDuDossier = $form->getData()["nom"];
+            //4- Créer le dossier
+            $fs=new Filesystem();
+            $fs->mkdir("Photos/$nomDuDossier");
+            //5- Rediriger vers la page d'accueil
+            return $this->redirectToRoute("app_home", ["nomDuDossier" => $nomDuDossier]);
+        }
+
+        $finder = new Finder();
         $finder->directories()->in('Photos');
+
         return $this->render('home/index.html.twig', [
             "dossiers" => $finder,
+            "formulaire" => $form->createView(),
         ]);
     }
 
     public function menu(): Response
     {
-        $finder= new Finder();
+        $finder = new Finder();
         $finder->directories()->in('Photos');
         return $this->render('home/_menu.html.twig', [
             "dossiers" => $finder,
